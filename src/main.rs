cc569f275f80b64c8d9dd7d1e6fba3ced3330ce7
Andrew Kvalheim <Andrew@Kvalhe.im>
2023-01-16 20:46:42 -0800

    Add speed and dithering sliders

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,31 @@ use fltk::{
 };
 use rgb::AsPixels;
 use std::path::PathBuf;
-use std::sync::atomic::{AtomicU8, Ordering::Relaxed};
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc, Arc, RwLock};
 use std::thread;
 
-#[derive(Parser, Debug)]
+#[derive(Debug, Parser)]
 #[command(version)]
 struct Args {
     #[arg()]
     path: PathBuf,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Params {
+    pub dithering: u8,
+    pub quality: u8,
+    pub speed: u8,
+}
+
+impl Default for Params {
+    fn default() -> Self {
+        Self {
+            dithering: 0,
+            quality: 25,
+            speed: 1,
+        }
+    }
 }
 
 // Pending https://github.com/rust-lang/rust/issues/67057
@@ -28,20 +44,33 @@ fn u8_from_f64(n: f64) -> u8 {
 }
 
 fn main() -> Result<()> {
-    // Initialize app
     let args = Args::parse();
+    let params = Arc::new(RwLock::new(Params::default()));
+    let (tx, rx) = mpsc::channel();
+
+    // Initialize GUI
     let app = App::default();
     let mut window = Window::default().with_size(640, 360 + 24 + 4 * 2);
-    let mut frame = Frame::default().size_of(&window);
-    let mut slider = HorNiceSlider::default()
-        .with_pos(4, 360 + 4)
-        .with_size(640 - 4 * 2, 24);
-    slider.set_minimum(0.0);
-    slider.set_maximum(100.0);
-    slider.set_step(1.0, 1);
-    slider.set_value(50.0);
-    window.end();
     window.make_resizable(true);
+    let mut frame = Frame::default().size_of(&window);
+    macro_rules! slider {
+        ($param:ident, $min:expr, $max:expr, $x:expr, $y:expr, $w:expr, $h:expr) => {{
+            let (tx, params) = (tx.clone(), params.clone());
+            let mut slider = HorNiceSlider::default().with_pos($x, $y).with_size($w, $h);
+            slider.set_minimum($min.into());
+            slider.set_maximum($max.into());
+            slider.set_step(1.0, 1);
+            slider.set_value(params.read().unwrap().$param.into());
+            slider.set_callback(move |s| {
+                params.write().unwrap().$param = u8_from_f64(s.value());
+                tx.send(()).unwrap();
+            });
+        }};
+    }
+    slider!(speed, 1, 10, 4, 364, 640 / 4 - 4 - 2, 24);
+    slider!(quality, 0, 100, 640 / 4 + 2, 364, 640 / 2 - 4 - 2, 24);
+    slider!(dithering, 0, 10, 640 / 4 * 3 + 2, 364, 640 / 4 - 4 - 2, 24);
+    window.end();
     window.show();
 
     // Load source
@@ -54,65 +83,57 @@ fn main() -> Result<()> {
     // Initialize quantizer
     let mut quantizer = imagequant::new();
 
-    // Create worker thread
-    let awaiting = Arc::new(AtomicU8::new(u8_from_f64(slider.value())));
-    let ((tx, rx), target) = (mpsc::channel(), awaiting.clone());
-    thread::spawn(move || {
+    // Initialize worker
+    thread::spawn(move || -> Result<()> {
         let mut displayed = None;
 
         loop {
-            let _: () = rx.recv().unwrap();
-            let quality = target.load(Relaxed);
-            if displayed == Some(quality) {
-                continue;
-            };
+            rx.recv()?;
+            let working = params.read().unwrap().clone();
+            match &displayed {
+                Some(d) if d == &working => continue,
+                _ => {}
+            }
+            macro_rules! abort_if_untargeted {
+                () => {
+                    if *params.read().unwrap() != working {
+                        continue;
+                    }
+                };
+            }
 
             // Quantize
-            quantizer.set_speed(10).unwrap();
-            quantizer.set_quality(0, quality).unwrap();
-            let mut source_pixels = quantizer
-                .new_image_borrowed(
-                    source_rgba.as_pixels(),
-                    source.width() as usize,
-                    source.height() as usize,
-                    0.0,
-                )
-                .unwrap();
-            if target.load(Relaxed) != quality {
-                continue;
-            };
-            let mut quantization = quantizer.quantize(&mut source_pixels).unwrap();
-            if target.load(Relaxed) != quality {
-                continue;
-            };
-            quantization.set_dithering_level(0.0).unwrap();
-            let (palette, quantized_indexed) = quantization.remapped(&mut source_pixels).unwrap();
-            if target.load(Relaxed) != quality {
-                continue;
-            };
+            quantizer.set_quality(0, working.quality)?;
+            quantizer.set_speed(working.speed.into())?;
+            let mut source_pixels = quantizer.new_image_borrowed(
+                source_rgba.as_pixels(),
+                source.width() as usize,
+                source.height() as usize,
+                0.0,
+            )?;
+            abort_if_untargeted!();
+            let mut quantization = quantizer.quantize(&mut source_pixels)?;
+            abort_if_untargeted!();
+            quantization.set_dithering_level(f32::from(working.dithering) / 10.0)?;
+            let (palette, quantized_indexed) = quantization.remapped(&mut source_pixels)?;
+            abort_if_untargeted!();
 
             // Display
             let quantized_rgba = quantized_indexed
                 .iter()
                 .flat_map(|&i| palette[i as usize].iter())
                 .collect::<Vec<_>>();
-            let image = RgbImage::new(&quantized_rgba, source.w(), source.h(), Rgba8).unwrap();
-            if target.load(Relaxed) != quality {
-                continue;
-            };
-            displayed.replace(quality);
+            let image = RgbImage::new(&quantized_rgba, source.w(), source.h(), Rgba8)?;
+            abort_if_untargeted!();
+            displayed.replace(working);
             frame.set_image(Some(image));
             frame.redraw();
             app::awake();
         }
     });
-    slider.set_callback(move |s| {
-        awaiting.store(u8_from_f64(s.value()), Relaxed);
-        tx.send(()).unwrap();
-    });
 
     // Run
-    slider.do_callback();
+    tx.send(())?;
     app.run()?;
     Ok(())
 }
